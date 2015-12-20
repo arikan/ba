b4570638f7bc4ae2a40fbe00c5f0b0b5f17e43bc
arikan <arikan@gmail.com>
2015-12-20 02:30:38 +0200

    thumb title and mobile paddings

--- templates/_thumb-module.php
+++ templates/_thumb-module.php
@@ -1,12 +1,14 @@
 <a href="<?php the_permalink() ?>">
     <? the_post_thumbnail('medium', array( 'class' => 'img-responsive', 'title' => the_title_attribute( 'echo=0' ) )); ?>
 </a>
-<h3>
-    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-</h3>
-<?php
-    $category = get_the_category();
-    if ($category) {
-      echo '<time class="published"><a href="'.esc_url( home_url( '/' ) ) . 'works' . '/' . $category[1]->slug . '/" title="' . sprintf( __( "View all works in %s" ), $category[1]->name ) . '" ' . '>' . $category[1]->name.'</a></time>';
-    }
-?>
+<div class="caption">
+    <h3>
+        <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+    </h3>
+    <?php
+        $category = get_the_category();
+        if ($category) {
+          echo '<time class="published"><a href="'.esc_url( home_url( '/' ) ) . 'works' . '/' . $category[1]->slug . '/" title="' . sprintf( __( "View all works in %s" ), $category[1]->name ) . '" ' . '>' . $category[1]->name.'</a></time>';
+        }
+    ?>
+</div>
