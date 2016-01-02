f3129f6921abb97c9133a330827590d7b32de021
arikan <arikan@gmail.com>
2016-01-02 19:30:17 +0200

    search paddings aligned for desktop and mobile

--- templates/searchform.php
+++ templates/searchform.php
@@ -2,6 +2,6 @@
     <form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url(home_url('/')); ?>">
         <label class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field typeahead" placeholder="<?php _e('Search', 'roots'); ?>" lang="en" tabindex="1" autocomplete="off" spellcheck="false" dir="auto" autofocus>
-        <a href="" class="close-search">x</a>
+        <a href="" class="close-search">&times;</a>
     </form>
 </div>
