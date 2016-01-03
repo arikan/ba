1e39c5ef620cb14f09372ba338377cb6d523a26b
arikan <arikan@gmail.com>
2016-01-03 20:22:07 +0200

    embed responsive

--- templates/content-single.php
+++ templates/content-single.php
@@ -5,33 +5,33 @@
             <?php get_template_part('templates/entry-meta'); ?>
         </header>
         <div class="entry-content">
-            <div class="row">
-                <div class="col-sm-4">
-                    <?php
-                        $category = get_the_category();
-                        if ($category) {
-                            echo '<time class="published"><a href="'.esc_url( home_url( '/' ) ) . 'works' . '/' . $category[1]->slug . '/" title="' . sprintf( __( "View all works in %s" ), $category[1]->name ) . '" ' . '>' . $category[1]->name.'</a></time>';
-                        }
-                    ?>
-                    <?php $view_interactive = get_post_meta($post->ID, "view_interactive", true); ?>
-                    <?php if($view_interactive) { ?>
-                        <span class="view-interactive">
-                            <?php echo $view_interactive; ?>
-                        </span>
-                    <?php } ?>
+            <div class="info">
+                <?php
+                    $category = get_the_category();
+                    if ($category) {
+                        echo '<time class="published"><a href="'.esc_url( home_url( '/' ) ) . 'works' . '/' . $category[1]->slug . '/" title="' . sprintf( __( "View all works in %s" ), $category[1]->name ) . '" ' . '>' . $category[1]->name.'</a></time>';
+                    }
+                ?>
+
+                <div class="entry-tags">
+                    <?php the_tags("", ", ", $after ); ?>
                 </div>
-                <div class="col-sm-8">
-                    <div class="entry-tags">
-                        <?php the_tags("", ", ", $after ); ?>
-                    </div>
-                </div>
+
+                <?php $view_interactive = get_post_meta($post->ID, "view_interactive", true); ?>
+                <?php if($view_interactive) { ?>
+                    <span class="view-interactive">
+                        <a href="<?php echo $view_interactive; ?>" target="_blank">View interactive</a>
+                    </span>
+                <?php } ?>
             </div>
 
             <?php the_content(); ?>
         </div>
+
         <footer>
             <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
         </footer>
+
         <?php comments_template('/templates/comments.php'); ?>
     </article>
 <?php endwhile; ?>
