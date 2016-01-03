387e788015317a81725c36ca19c44fc95c1ae384
arikan <arikan@gmail.com>
2016-01-03 22:02:56 +0200

    header not displayed if post with feature imge

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,9 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class(); ?>>
-        <header>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-            <?php get_template_part('templates/entry-meta'); ?>
-        </header>
+        <?php if ( !is_singular() || !has_post_thumbnail( $post->ID ) ) { ?>
+            <header>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <?php get_template_part('templates/entry-meta'); ?>
+            </header>
+        <?php } ?>
+
         <div class="entry-content">
             <div class="info">
                 <?php
