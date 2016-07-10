a19eac4ac204356ed80f6a7db8960139b9cef2db
arikan <arikan@gmail.com>
2016-07-10 16:39:21 +0300

    works image blend

--- templates/_thumb-module.php
+++ templates/_thumb-module.php
@@ -2,7 +2,7 @@
 <?php $image_url = $thumb[0] ?>
 
 <a href="<?php the_permalink() ?>">
-    <div class="thumb-as-bg" style="background: url(<?php echo $image_url; ?>); background-size: cover; background-repeat: no-repeat; background-position: center; background-color: #DDDDDD;"></div>
+    <div class="thumb-as-bg" style="background: url(<?php echo $image_url; ?>); background-size: cover; background-repeat: no-repeat; background-position: center; background-color: #f5e6db;"></div>
 </a>
 <div class="caption">
     <h3>
