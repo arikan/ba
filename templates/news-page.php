42ff72c8f93661d8c6963bb5aad7ec4251bf43a7
arikan <arikan@gmail.com>
2016-08-06 15:55:53 +0300

    news list refined

--- templates/news-page.php
+++ templates/news-page.php
@@ -4,16 +4,18 @@
  */
 ?>
 
-<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; query_posts("cat=3&paged=$paged"); ?>
+<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; query_posts("cat=2&paged=$paged"); ?>
 
 <?php while (have_posts()) : the_post(); ?>
     <div class="post">
-        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <h2>
+            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+        </h2>
 		<div class="subtitle">
 			<?php the_time('F j, Y'); ?>
-			<?php the_tags(' | <b>Tags:</b> ', ', ', ''); ?>
-			<?php if ( $user_ID ) : ?> | <?php edit_post_link("Edit"); ?> <?php endif; ?> |
-			<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+			<?php the_tags(' | ', ', ', ''); ?>
+			<?php if ( $user_ID ) : ?> | <?php edit_post_link("Edit"); ?> <?php endif; ?>
+            <!-- | <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>-->
 		</div>
 
         <?php the_excerpt(); ?>
