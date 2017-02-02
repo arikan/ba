d978ff495c2ed21850cd857a68a2eb2cf8a7d945
arikan <arikan@gmail.com>
2017-02-02 13:46:00 -0500

    https redirect added

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,14 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php global $wp; $current_url = home_url(add_query_arg(array(),$wp->request)); ?>
+  <link rel="canonical" href="<?php echo $current_url; ?>" />
+  <script type="text/javascript">
+    if (window.location.protocol != "https:") {
+      window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    }
+  </script>
+
   <?php wp_head(); ?>
 
 </head>
